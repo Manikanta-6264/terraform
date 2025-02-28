ddd8dcc3e468afb6cbedca08f79138236bb44d53
Manikanta-6264 <manikantagembali1@gmail.com>
2025-02-28 16:11:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 provider "aws" {
+  region = "ap-south-1" # Mumbai region
 }
-resource "aws_instance" "one" {
-ami = "ami-0d682f26195e9ec0f"
-instance_type = "t2.micro"
-tags = {
-Name = "sussu-server"
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d682f26195e9ec0f" # Replace with a suitable AMI for Mumbai region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "manikanta-server"
+  }
 }
-}
+
